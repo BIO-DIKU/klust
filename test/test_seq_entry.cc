025e19323536f23391e4bd9cf97dc0336914ee97
Anders Kiel Hovgaard <akhovg@gmail.com>
2015-10-01 20:35:34 +0200

    made existing Sequence tests mostly work with SeqEntry

--- test/test_seq_entry.cc
+++ test/test_seq_entry.cc
@@ -22,74 +22,72 @@
 
 #include "../src/seq_entry.cc"
 
-/*
 TEST_CASE("sequences can be constructed, copied and moved", "[sequence]") {
   SeqEntry t1;
-  SeqEntry t2 = SeqEntry("Name", "Sequence", "Scores", SeqEntry::SeqType::dna);
+  SeqEntry t2 = SeqEntry("Name", "Sequence", {}, SeqEntry::SeqType::protein);
 
-  REQUIRE(t1.seq_name() == "");
+  REQUIRE(t1.name() == "");
   REQUIRE(t1.seq() == "");
-  REQUIRE(t1.scores() == "");
-  REQUIRE(t1.type() == SeqEntry::SeqType::unknown);
+  REQUIRE(t1.scores().empty());
+  REQUIRE(t1.type() == SeqEntry::SeqType::nucleotide);
 
-  REQUIRE(t2.seq_name() == "Name");
+  REQUIRE(t2.name() == "Name");
   REQUIRE(t2.seq() == "Sequence");
-  REQUIRE(t2.scores() == "Scores");
-  REQUIRE(t2.type() == SeqEntry::SeqType::dna);
+  REQUIRE(t2.scores().empty());
+  REQUIRE(t2.type() == SeqEntry::SeqType::protein);
 
   SECTION("Copy SeqEntry 2 to SeqEntry 1") {
     t1 = t2;
 
-    REQUIRE(t1.seq_name() == "Name");
+    REQUIRE(t1.name() == "Name");
     REQUIRE(t1.seq() == "Sequence");
-    REQUIRE(t1.scores() == "Scores");
-    REQUIRE(t1.type() == SeqEntry::SeqType::dna);
+    REQUIRE(t1.scores().empty());
+    REQUIRE(t1.type() == SeqEntry::SeqType::protein);
   }
 
   SECTION("Use copy contructor for new SeqEntry") {
     SeqEntry t3(t2);
 
-    REQUIRE(t3.seq_name() == "Name");
+    REQUIRE(t3.name() == "Name");
     REQUIRE(t3.seq() == "Sequence");
-    REQUIRE(t3.scores() == "Scores");
-    REQUIRE(t3.type() == SeqEntry::SeqType::dna);
+    REQUIRE(t3.scores().empty());
+    REQUIRE(t3.type() == SeqEntry::SeqType::protein);
   }
 
   SECTION("Use move contructor for new SeqEntry") {
     SeqEntry t4 = std::move(t2);
 
-    REQUIRE(t4.seq_name() == "Name");
+    REQUIRE(t4.name() == "Name");
     REQUIRE(t4.seq() == "Sequence");
-    REQUIRE(t4.scores() == "Scores");
-    REQUIRE(t4.type() == SeqEntry::SeqType::dna);
+    REQUIRE(t4.scores().empty());
+    REQUIRE(t4.type() == SeqEntry::SeqType::protein);
 
-    REQUIRE(t2.seq_name() == "");
+    REQUIRE(t2.name() == "");
     REQUIRE(t2.seq() == "");
-    REQUIRE(t2.scores() == "");
-    REQUIRE(t2.type() == SeqEntry::SeqType::dna);
+    REQUIRE(t2.scores().empty());
+    REQUIRE(t2.type() == SeqEntry::SeqType::protein);
   }
 }
 
 TEST_CASE("subsequences can be extracted from sequences", "[sequence]") {
-  SeqEntry t1 = SeqEntry("Name", "Sequence", "Scores", SeqEntry::SeqType::rna);
+  SeqEntry t1 = SeqEntry("Name", "Sequence", {}, SeqEntry::SeqType::nucleotide);
 
-  REQUIRE(t1.seq_name() == "Name");
+  REQUIRE(t1.name() == "Name");
   REQUIRE(t1.seq() == "Sequence");
-  REQUIRE(t1.scores() == "Scores");
-  REQUIRE(t1.type() == SeqEntry::SeqType::rna);
+  REQUIRE(t1.scores().empty());
+  REQUIRE(t1.type() == SeqEntry::SeqType::nucleotide);
 
   SeqEntry t2 = t1.SubSeq(2, 4);
 
   // t1 stays unchanged
-  REQUIRE(t1.seq_name() == "Name");
+  REQUIRE(t1.name() == "Name");
   REQUIRE(t1.seq() == "Sequence");
-  REQUIRE(t1.scores() == "Scores");
-  REQUIRE(t1.type() == SeqEntry::SeqType::rna);
+  REQUIRE(t1.scores().empty());
+  REQUIRE(t1.type() == SeqEntry::SeqType::nucleotide);
 
   // t2 contains expected subsequence of t1 and they are otherwise identical
-  REQUIRE(t2.seq_name() == "Name");
+  REQUIRE(t2.name() == "Name");
   REQUIRE(t2.seq() == "quen");
-  REQUIRE(t2.scores() == "Scores");
-  REQUIRE(t2.type() == SeqEntry::SeqType::rna);
+  REQUIRE(t2.scores().empty());
+  REQUIRE(t2.type() == SeqEntry::SeqType::nucleotide);
 }
-*/
